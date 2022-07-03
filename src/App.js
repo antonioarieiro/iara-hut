29a86a9fb4d7aaa56cf44451a8f26a31b20d0df1
antonioarieiro <sfgfffs@gmail.com>
2022-07-02 21:54:03 -0300

    create inital struc routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { IaraHutProvider } from './Context/IaraHutProvider';
+import Pages from './Pages';
+import Component from './Components/index';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <IaraHutProvider>
+        <Component.Cart />
+        <Component.Details />
+        <Component.Header />
+        <div className="default-home overflow-hidden">
+          <Routes>
+            <Route path='/' element={<Pages.Home />} />
+          </Routes>
+        </div>
+      </IaraHutProvider>
+    </>
   );
 }
-
-export default App;
